146a72640c8c7f3d2580a359c6a15972b31318d2
Aditya <paaditya@amd.com>
2024-09-26 14:20:29 +0530

    Added sleep function to run.sh
    Signed-off-by: Aditya <paaditya@amd.com>

--- run.sh
+++ run.sh
@@ -3,18 +3,24 @@
 # Get the distribution name
 distro=$(cat /etc/os-release | grep ^ID= | cut -d'=' -f2)
 
+echo " "
 # Print the distribution name
-echo "Distribution: $distro"
+#echo "Distribution: $distro"
 loc=$(pwd)
-echo "$loc"
+echo "Working directory choosen for lkp is: $loc"
+echo " "
+echo "/////////////=== To stop this current process of creating LKP, use CTRL+C ===//////////////"
+echo " "
+sleep 2
 # Check if the distro is Ubuntu
 if [ "$distro" == "ubuntu" ]; then
   echo "Found $distro on this system, installing required dependencies"
+  echo " "
   sudo $loc/ubuntu-lkp-automation.sh
 else
   echo "Found $distro on this system. installing required dependencies"
+  echo " "
   sudo $loc/centos-lkp-automation.sh
-
 fi
 
 
