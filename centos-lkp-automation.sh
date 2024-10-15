a22e95fad05cde70ca8f8c75b084c03b7422ed9b
PvsNarasimha <PVS.NarasimhaRao@amd.com>
2024-10-15 13:20:21 +0800

    Resolved minimal issues with taking user input for service file creation
    Signed-off-by: PvsNarasimha <PVS.NarasimhaRao@amd.com>

--- centos-lkp-automation.sh
+++ centos-lkp-automation.sh
@@ -14,8 +14,12 @@ check_exit() {
 # Signal handling to allow graceful exit on Ctrl+C
 trap "echo 'Caught SIGINT. Exiting...'; exit 0" SIGINT
 
-read -p "Do you want to create a service file for the lkp running? (yes/y): " servi
-service=$(echo "$servi" | tr '[:upper:]' '[:lower:]')
+read -p "Do you want to create a service file for the lkp running? (yes/y): " servi < /dev/tty
+servi=$(echo "$servi" | tr '[:upper:]' '[:lower:]')
+
+
+
+
 
 echo " "
 echo "============================================"
